e926061c926a246bf5b1cf7a914a6d6ae70a9ca8
alexmoskau68-creator <alexmoskau68@gmail.com>
2026-09-14 22:03:57 +0300

    Add project field to expenses

--- app/src/main/java/ru/controlexpenses/Expense.java
+++ app/src/main/java/ru/controlexpenses/Expense.java
@@ -6,15 +6,21 @@ public class Expense {
     public String merchant;
     public String category;
     public String currency;
+    public String project;
     public String note;
     public double amount;
 
     public Expense(long id, String date, String merchant, String category, String currency, String note, double amount) {
+        this(id,date,merchant,category,currency,"",note,amount);
+    }
+
+    public Expense(long id, String date, String merchant, String category, String currency, String project, String note, double amount) {
         this.id = id;
         this.date = date;
         this.merchant = merchant;
         this.category = category;
         this.currency = currency;
+        this.project = project == null ? "" : project;
         this.note = note;
         this.amount = amount;
     }
